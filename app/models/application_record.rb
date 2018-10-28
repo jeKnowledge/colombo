183eb857a897f4748d52f2444a3b0b7346c784c0
Goncalo Amaral <goa.docs@gmail.com>
2018-10-28 20:45:18 +0000

    sign_in form

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def render_errors
-    if self.errors.any?
-      self.errors.full_messages.each do |message|
-        <li><%= message %></li>
-      end
-    end
-  end
 end
