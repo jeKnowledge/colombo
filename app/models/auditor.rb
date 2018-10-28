a05a1bec97d7d177aaf7adef1335163139ec0c3f
Goncalo Amaral <goa.docs@gmail.com>
2018-10-28 23:04:54 +0000

    audotor sign_up

--- app/models/auditor.rb
+++ app/models/auditor.rb
@@ -3,4 +3,10 @@ class Auditor < ApplicationRecord
 
   validates_presence_of :email, :rating, :qualifications, :cv
   validates_uniqueness_of :email
+
+  after_new :generate_username
+
+  def generate_username
+    self.username = "a_#{self.first_name[0..2].downcase}#{self.last_name[0..2].downcase}_#{self.id}"
+  end
 end
