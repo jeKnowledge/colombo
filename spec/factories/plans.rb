a96bb3ff87648708b5c6745b40efdf89de8474fb
Tiago Martins <tiagomartins282@gmail.com>
2018-10-31 15:25:16 +0000

    show validated field when listing reports and plans

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :plan do
     site { Faker::Internet.domain_name }
     address { Faker::Address.street_name }
-    date { Faker::Date.between(2.days.ago, Date.today) }
+    date { Faker::Date.forward(14) }
     products { Faker::Lorem.sentence(3) }
     auditor { FactoryBot.create(:auditor) }
   end
