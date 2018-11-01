3f3d0d2e866e8d764691d7098f24e9dc953df3a3
Tiago Martins <tiagomartins282@gmail.com>
2018-11-01 01:26:21 +0000

    rename terms to consent

--- db/migrate/20181101012513_create_consents.rb
+++ db/migrate/20181101012513_create_consents.rb
@@ -1,6 +1,6 @@
-class CreateConserns < ActiveRecord::Migration[5.2]
+class CreateConsents < ActiveRecord::Migration[5.2]
   def change
-    create_table :conserns do |t|
+    create_table :consents do |t|
       t.string :title, null: false, default: ""
       t.text :description, null: false, default: ""
       t.timestamps
