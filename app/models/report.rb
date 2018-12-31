6dfbd50270b2a2d5cd03c72e6b2ac6582d5b1a15
Goncalo Amaral <goa.docs@gmail.com>
2018-12-31 16:51:11 +0000

    Create upload method

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,14 @@
 class Report < Audit
+  # Relationships
   has_many :purchases, dependent: :destroy
+
+  # Uploader
   mount_uploader :report, ReportUploader
 
+  # Validations
   validates_presence_of :rating, :report
 
+  # Methods
   def rating
     if rating_sum != 0
       return rating_sum / self.ratings
@@ -17,5 +22,10 @@ class Report < Audit
     self.rating_sum += rating
     self.ratings += 1
     self.save
+  end
+
+  def upload_date
+    timestamp = self.report_identifier.split("_")[1].split(".")[0].to_i
+    Time.at(timestamp).to_date
   end
 end
