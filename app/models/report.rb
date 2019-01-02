241f3d06d6f58765ce3930ef4821291cb02ae694
Goncalo Amaral <goa.docs@gmail.com>
2019-01-02 16:31:20 +0000

    Moved rating setting and getting from audit to report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,16 @@
 class Report < Audit
+  @@DEFAULT_RATING = 0
+
+  # Relationships
   has_many :purchases, dependent: :destroy
+
+  # Uploaders
   mount_uploader :report, ReportUploader
 
+  # Validations
   validates_presence_of :rating, :report
 
+  # Methods
   def rating
     if rating_sum != 0
       return rating_sum / self.ratings
@@ -17,5 +24,13 @@ class Report < Audit
     self.rating_sum += rating
     self.ratings += 1
     self.save
+  end
+
+  def self.set_default_rating(rating)
+    @@DEFAULT_RATING = rating
+  end
+
+  def self.get_default_rating
+    @@DEFAULT_RATING
   end
 end
